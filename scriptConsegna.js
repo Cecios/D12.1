52bd6a8b486b7922077ef7901b9e18e43bcfea5a
Cecios <andrea.raneri94@gmail.com>
2023-12-26 00:13:15 +0100

    Consegna 1,3

--- scriptConsegna.js
+++ scriptConsegna.js
@@ -1,8 +1,25 @@
 //ESERCIZIO 1
+console.log("Esercizio 1:");
 
-const check50 = function(x,y){
+const checkSum = function(x,y){
     return (x+y === 50 || x === 50 || y === 50) 
 }
-console.log(check50(25,25));
+console.log(checkSum(25,25));
 
 // ESERCIZIO 2
+console.log("Esercizio 2:");
+const strRemove = function(){
+
+}
+
+// ESERCIZIO 3
+console.log("Esercizio 3:");
+const checkInterval = function(x,y){
+    return ( 40 <= x && x <= 60) || (70 <= x && x <= 100) || (40 <= y && y <= 60) || (70 <= y && y <= 100)
+}
+console.log(checkInterval(20,30));
+
+// ESERCIZIO 4
+console.log("Esercizio 4:");
+
+
