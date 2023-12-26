af6a2b1fafdf5a19ae7f1f5edbfdabbba57d2530
Cecios <andrea.raneri94@gmail.com>
2023-12-26 01:03:24 +0100

    CONSEGNA 4,5

--- scriptConsegna.js
+++ scriptConsegna.js
@@ -21,5 +21,24 @@ console.log(checkInterval(20,30));
 
 // ESERCIZIO 4
 console.log("Esercizio 4:");
+const checkCity = function(city){
+    if (city.startsWith("Los") || city.startsWith("New") ){
+        return city
+    }
+    else{
+        return false
+    }
+}
 
+console.log(checkCity("New pippo"));
 
+// ESERCIZIO 5
+let array = [1,2,5,14]
+const sumArray = function(arr){
+    let c = 0
+    for (let i = 0; i < arr.length; i++) {
+         c += arr[i]
+    }
+    return c
+}
+console.log(sumArray(array));
